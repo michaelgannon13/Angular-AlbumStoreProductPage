94e7d48e060409f47cda2fa80431f958ed3d7ddc
Michael <m.j.gannon@accenture.com>
2018-06-12 10:25:58 +0100

    bootstrap added to list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,7 +14,6 @@ export class ProductService {
 
   constructor(private _http: Http) {  }
 
-  // return type is of type Observable
   getAlbum (id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
     <Album>response.json());
